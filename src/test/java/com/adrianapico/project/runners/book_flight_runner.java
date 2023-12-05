0de513565821d8094bfea0e1d28a479ca44f20d9
adrianapico <adriana.s.pico.c@gmail.com>
2023-12-05 16:26:35 -0500

    changes

--- src/test/java/com/adrianapico/project/runners/book_flight_runner.java
+++ src/test/java/com/adrianapico/project/runners/book_flight_runner.java
@@ -1,14 +1,14 @@
 package com.adrianapico.project.runners;
 
+
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = {"src/test/resources/features/book_flight.feature"},
-        glue = {"com.adrianapico.project.step_definitions.book_flight_step_definitions"},
+        glue = {"com/adrianapico/project/step_definitions"},
         snippets = CucumberOptions.SnippetType.UNDERSCORE
 )
 
